46a9e698f0a4721c330cb486d78055f4e8d68f07
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-04-28 22:04:47 -0500

    unit tests for the Year class

---
+++ lib/year.rb
@@ -0,0 +1,16 @@
+
+class Year
+
+  attr_reader :year
+
+  MONTH = ['January','February','March','April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+  def initialize(year)
+    @year = year
+  end
+
+  def year_cal
+  end
+
+
+end
